e511d88e3028fef25cf6997594ab7d6e871584a6
Berwout de Vries Robles <berwout.devriesrobles@sanoma.com>
2020-03-21 17:24:00 +0100

    fix builds

--- pipeline/maven.groovy
+++ pipeline/maven.groovy
@@ -1,7 +1,7 @@
 return {
     stage("Building ${PRODUCT_NAME}") {
         docker.image('maven:3').inside() {
-            "mvn clean package"
+            sh "mvn clean package"
         }
     }
 }
--- pipeline/npm.groovy
+++ pipeline/npm.groovy
@@ -1,6 +1,7 @@
 return {
     stage("Building ${PRODUCT_NAME}") {
         docker.image('node:13').inside() {
+            sh "npm install"
             sh "ng build --prod"
         }
     }
