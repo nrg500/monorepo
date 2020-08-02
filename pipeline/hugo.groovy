4d0a8d1ddd872c3c787b835236183e4642401644
Berwout de Vries Robles <bdvr@live.nl>
2020-08-02 17:57:21 +0200

    clear entrypoint for hugo

--- pipeline/hugo.groovy
+++ pipeline/hugo.groovy
@@ -1,7 +1,7 @@
 def apply(product) {
     return {
         stage("Building ${product}") {
-            docker.image('klakegg/hugo:0.74.3 shell').inside() {
+            docker.image('klakegg/hugo:0.74.3').inside("--entrypoint=") {
                 dir("products/${product}") {
                     sh "hugo"
                     stash name: "${product}", includes: "public/*"
