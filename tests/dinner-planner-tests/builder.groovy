5eda13da319393fc21b865768eac4d5c3914318c
Berwout de Vries Robles <bdvr@live.nl>
2020-08-24 00:12:12 +0200

    add pre-removal of selenium / network

--- tests/dinner-planner-tests/builder.groovy
+++ tests/dinner-planner-tests/builder.groovy
@@ -2,24 +2,26 @@ def build() {
     return {
         stage("Run selenium container") {
             try {
-            sh "docker network create selenium"
-            sh "docker run -d -p 4444:4444 --network selenium --name selenium -v /dev/shm:/dev/shm selenium/standalone-chrome:4.0.0-alpha-6-20200730"
-             docker.image('maven:3').inside("--network selenium") {
-                dir("tests/dinner-planner-tests") {
-                    sh "mvn -B -DargLine='-DTEST_URI=https://berwout.nl -DSELENIUM_URI=http://selenium:4444/' clean test"
+                try {
+                    sh "docker rm -f selenium"
+                } catch (Exception e) {}
+                try {
+                    sh "docker network rm selenium"
+                } catch (Exception e) {}
+                sh "docker network create selenium"
+                sh "docker run -d -p 4444:4444 --network selenium --name selenium -v /dev/shm:/dev/shm selenium/standalone-chrome:4.0.0-alpha-6-20200730"
+                docker.image('maven:3').inside("--network selenium") {
+                    dir("tests/dinner-planner-tests") {
+                        sh "mvn -B -DargLine='-DTEST_URI=https://berwout.nl -DSELENIUM_URI=http://selenium:4444/' clean test"
+                    }
                 }
-             }
             } finally {
                 try {
                     sh "docker rm -f selenium"
-                } catch (Exception e) {
-
-                }
+                } catch (Exception e) {}
                 try {
                     sh "docker network rm selenium"
-                } catch (Exception e) {
-
-                }
+                } catch (Exception e) {}
             }
         }
     }
