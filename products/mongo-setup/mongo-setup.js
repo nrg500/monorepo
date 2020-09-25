54695d247771b1dd46be0c3563272e9a8554c2c8
nrg500 <bdvr@live.nl>
2020-09-25 08:03:33 +0200

    Changed dinner-planner tests and mongo-setup (#2)
    * fix reference to deploy yaml files
    
    * englishify dinner-planner tests
    
    Co-authored-by: Berwout de Vries Robles <berwout.devriesrobles@sanoma.com>

--- products/mongo-setup/mongo-setup.js
+++ products/mongo-setup/mongo-setup.js
@@ -1,5 +1,5 @@
 let conn = new Mongo("mongo:27017")
 let db = conn.getDB("ingredients")
 db.createCollection('ingredients');
-db.ingredients.update({"name":"Verse geitenkaas","amount":200,"unit":"gr"}, {"name":"Verse geitenkaas","amount":200,"unit":"gr"}, {upsert: true})
-db.ingredients.update({"name":"Geraspte jonge kaas","amount":175,"unit":"gr"}, {"name":"Geraspte jonge kaas","amount":175,"unit":"gr"}, {upsert: true})
+db.ingredients.update({"name":"Radishes","amount":200,"unit":"gr"}, {"name":"Radishes","amount":200,"unit":"gr"}, {upsert: true})
+db.ingredients.update({"name":"Cucumber","amount":175,"unit":"gr"}, {"name":"Cucumber","amount":175,"unit":"gr"}, {upsert: true})
