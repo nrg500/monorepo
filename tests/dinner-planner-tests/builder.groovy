2f1fd5a72e19e4b08c3304e291b206c3fef3f32f
Berwout de Vries Robles <bdvr@live.nl>
2021-03-09 20:38:54 +0100

    rewrite monorepo to use kustomize

--- tests/dinner-planner-tests/builder.groovy
+++ tests/dinner-planner-tests/builder.groovy
@@ -6,15 +6,18 @@ def build() {
                     sh '''#!/bin/bash +e
                     docker rm -f selenium
                     docker network rm selenium
-                    kubectl delete job mongo-setup
-                    kubectl apply -f ../deploy/mongo-setup
                     docker network create selenium
                     docker run -d -p 4444:4444 --network selenium --name selenium -v /dev/shm:/dev/shm selenium/standalone-chrome:4.0.0-alpha-6-20200730
                     '''
                 } catch (Exception e) {}
                 docker.image('maven:3').inside("--network selenium") {
                     dir("tests/dinner-planner-tests") {
-                        sh "mvn -B -DargLine='-DTEST_URI=https://berwout.nl -DSELENIUM_URI=http://selenium:4444/' clean test"
+                        if(BRANCH_NAME != "main") {
+                            sh "mvn -B -DargLine='-DTEST_URI=https://${BRANCH_NAME}-dinner.berwout.nl -DSELENIUM_URI=http://selenium:4444/' clean test"
+                        } else {
+                            sh "mvn -B -DargLine='-DTEST_URI=https://dinner.berwout.nl -DSELENIUM_URI=http://selenium:4444/' clean test"
+                        }
+                       
                     }
                 }
             } finally {
