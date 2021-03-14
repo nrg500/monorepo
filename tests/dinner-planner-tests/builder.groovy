451f98a83b62d51608f793f44f6eea88eb3f3be4
Berwout de Vries Robles <bdvr@live.nl>
2021-03-14 14:53:05 +0100

    add namespace name

--- tests/dinner-planner-tests/builder.groovy
+++ tests/dinner-planner-tests/builder.groovy
@@ -18,8 +18,8 @@ def build() {
                 writeYaml file: "job.yaml", data: jobYaml, overwrite: true
                 sh "kubectl apply -f job.yaml"
                 sh "kubectl wait --for=condition=complete --timeout=30s job/${jobName}"
-                def pods = sh(returnStdout: true, script: "kubectl get pods --selector=job-name=${jobName} --output=jsonpath='{.items[*].metadata.name}'")
-                sh "kubectl logs $pods"
+                def pods = sh(returnStdout: true, script: "kubectl get pods --selector=job-name=${jobName} --output=jsonpath='{.items[*].metadata.name}' -ndefault")
+                sh "kubectl logs $pods -ndefault"
             }
         }
     }
