52067f5bf269e41985a22b845565daf534b8ec2a
Berwout de Vries Robles <bdvr@live.nl>
2021-03-14 12:54:19 +0100

    convert to test container

--- tests/dinner-planner-tests/builder.groovy
+++ tests/dinner-planner-tests/builder.groovy
@@ -1,32 +1,10 @@
 def build() {
     return {
         stage("Run selenium container") {
-            try {
-                try{ 
-                    sh '''#!/bin/bash +e
-                    docker rm -f selenium
-                    docker network rm selenium
-                    docker network create selenium
-                    docker run -d -p 4444:4444 --network selenium --name selenium -v /dev/shm:/dev/shm selenium/standalone-chrome:4.0.0-alpha-6-20200730
-                    '''
-                } catch (Exception e) {}
-                docker.image('maven:3').inside("--network selenium") {
-                    dir("tests/dinner-planner-tests") {
-                        if(BRANCH_NAME != "main") {
-                            sh "mvn -B -DargLine='-DTEST_URI=https://${BRANCH_NAME}-dinner.berwout.nl -DSELENIUM_URI=http://selenium:4444/' clean test"
-                        } else {
-                            sh "mvn -B -DargLine='-DTEST_URI=https://dinner.berwout.nl -DSELENIUM_URI=http://selenium:4444/' clean test"
-                        }
-                       
-                    }
-                }
-            } finally {
-                try{ 
-                    sh '''#!/bin/bash +e
-                    docker rm -f selenium
-                    docker network rm selenium
-                    '''
-                } catch(Exception e) {}
+            dir("tests/dinner-planner-tests") {
+                sh "docker build -t dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER}"
+                def test_uri = BRANCH_NAME == "main" ? "https://dinner.berwout.nl" : "https://${BRANCH_NAME}-dinner.berwout.nl"
+                sh "kubectl run -tty dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER} --image=dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER} --restart=Never --env=TEST_URI=${test_uri}"
             }
         }
     }
--- tests/dinner-planner-tests/src/test/java/nl/berwout/tests/StepDefinitions.java
+++ tests/dinner-planner-tests/src/test/java/nl/berwout/tests/StepDefinitions.java
@@ -30,11 +30,13 @@ public class StepDefinitions{
 
         caps.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         String seleniumUri = System.getProperty("SELENIUM_URI");
+        System.out.println("Selenium uri: " + seleniumUri);
         if(seleniumUri == null || seleniumUri.isEmpty()) {
             seleniumUri = "http://localhost:4444";
         };
         driver = new RemoteWebDriver(URI.create(seleniumUri).toURL(), caps);
         String testUri = System.getProperty("TEST_URI");
+        System.out.println("Test uri: " + testUri);
         if(testUri == null || testUri.isEmpty()) {
             testUri = "http://localhost:4200";
         };
