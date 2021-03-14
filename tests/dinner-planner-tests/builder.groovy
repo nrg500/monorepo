fad0cee85c9e66fb5095a9eed43f5ab2fc1b21a6
Berwout de Vries Robles <bdvr@live.nl>
2021-03-14 14:32:28 +0100

    run tests as job

--- tests/dinner-planner-tests/builder.groovy
+++ tests/dinner-planner-tests/builder.groovy
@@ -3,13 +3,20 @@ def build() {
         stage("Run selenium container") {
             dir("tests/dinner-planner-tests") {
                 def imageName = "berwoutv/dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER}"
-               def dockerImage = docker.build(imageName, ".")
+                def jobName = "dinner-planner-tests-${BRANCH_NAME}-${BUILD_NUMBER}"
+                def dockerImage = docker.build(imageName, ".")
                 docker.withRegistry('', 'dockerhub') {
                     dockerImage.push()
                 }
                 sh "docker rmi ${imageName}"
                 def test_uri = BRANCH_NAME == "main" ? "https://dinner.berwout.nl" : "https://${BRANCH_NAME}-dinner.berwout.nl"
-                sh "kubectl run -i --tty dinner-planner-tests-${BRANCH_NAME}-${BUILD_NUMBER} --image=berwoutv/dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER} --restart=Never --env=TEST_URI=${test_uri}"
+                def jobYaml = readYaml file: "job.yaml"
+                jobYaml.spec.template.spec.containers[0].env = [{"name": "test_uri", "value": test_uri}]
+                jobYaml.spec.template.spec.containers[0].image = imageName
+                jobYaml.metadata.name = jobName
+                jobYaml.spec.template.spec.containers[0].name = jobName
+                sh "kubectl apply -f job.yaml"
+                sh "kubectl wait --for=condition=complete --timeout=30s job/${jobName}"
             }
         }
     }
