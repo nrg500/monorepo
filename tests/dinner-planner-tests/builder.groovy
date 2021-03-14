8c8643160c28a6a60b6bcd02758794f5935a49f8
Berwout de Vries Robles <bdvr@live.nl>
2021-03-14 13:13:07 +0100

    fix selenium uri

--- tests/dinner-planner-tests/builder.groovy
+++ tests/dinner-planner-tests/builder.groovy
@@ -2,9 +2,14 @@ def build() {
     return {
         stage("Run selenium container") {
             dir("tests/dinner-planner-tests") {
-                sh "docker build -t dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER}"
+                def imageName = "berwoutv/dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER}"
+               def dockerImage = docker.build(imageName, ".")
+                docker.withRegistry('', 'dockerhub') {
+                    dockerImage.push()
+                }
+                sh "docker rmi ${imageName}"
                 def test_uri = BRANCH_NAME == "main" ? "https://dinner.berwout.nl" : "https://${BRANCH_NAME}-dinner.berwout.nl"
-                sh "kubectl run -tty dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER} --image=dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER} --restart=Never --env=TEST_URI=${test_uri}"
+                sh "kubectl run -i --tty dinner-planner-tests-${BRANCH_NAME}-${BUILD_NUMBER} --image=berwoutv/dinner-planner-tests:${BRANCH_NAME}-${BUILD_NUMBER} --restart=Never --env=TEST_URI=${test_uri}"
             }
         }
     }
