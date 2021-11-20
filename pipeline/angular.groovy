65dfb5f29fa20f1dcd0a0ca4743249ee81e33e4c
Berwout de Vries Robles <bdvr@live.nl>
2021-11-20 15:48:59 +0100

    revert to node 13 for angular build for now

--- pipeline/angular.groovy
+++ pipeline/angular.groovy
@@ -1,7 +1,7 @@
 def apply(product) {
     return {
         stage("Building ${product}") {
-            docker.image('node:latest').inside() {
+            docker.image('node:13').inside() {
                 dir("products/${product}") {
                     sh "npm install"
                     sh "npm install -g @angular/cli@latest"
