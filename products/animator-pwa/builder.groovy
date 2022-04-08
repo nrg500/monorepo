a3c78c42a5bc2abd63c45a8fb59df807bb2b38b6
Berwout de Vries Robles <berwout.de.vries.robles@openvalue.nl>
2022-04-08 21:04:29 +0200

    upload animator-pwa start.

---
+++ products/animator-pwa/builder.groovy
@@ -0,0 +1,9 @@
+def build(version) {
+  return {
+    stage("Uploading as docker image") {
+      def dockerBuild = load('pipeline/docker.groovy')
+      dockerBuild.buildAndUploadImage('berwoutv', "animator-pwa", version, '.')
+    }
+  }
+}
+return this
